3d701851c4288bb58fb7b2fccbd416ea0dbac6e7
uo271773 <uo271773@uniovi.es>
2021-12-01 07:56:49 +0100

    Validado HTML

--- PRACTICA3/Ejercicio-02/IdiomaNavegador.js
+++ PRACTICA3/Ejercicio-02/IdiomaNavegador.js
@@ -1,4 +1,3 @@
 /* IdiomaNavegador.js */
 // Version 1.0 08/11/2021 
-document.write("Idioma: ");
 document.write(infoNavegador.idioma);
--- PRACTICA3/Ejercicio-02/NombreNavegador.js
+++ PRACTICA3/Ejercicio-02/NombreNavegador.js
@@ -1,4 +1,3 @@
 /* NombreNavegador.js */
 // Version 1.0 08/11/2021 
-document.write("Nombre del navegador: ");
 document.write(infoNavegador.nombre);
